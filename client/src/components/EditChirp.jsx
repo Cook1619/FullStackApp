83125f2ebf9c5678d7ff4f821d90cd4ac909453f
Cook1619 <cook6348@gmail.com>
2018-07-09 08:16:31 -0500

    Setting up PUT

--- client/src/components/EditChirp.jsx
+++ client/src/components/EditChirp.jsx
@@ -1,16 +1,38 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 
 class EditChirp extends Component {
-    constructor(props){
-        super(props)
-        
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: '',
+            text: ''
+        }
     }
-    render(){
-        return(
+    async componentDidMount() {
+        try {
+            let res = await fetch('/api/chirps', {
+                method: 'PUT',
+            });
+            let data = await res.json();
+            console.log(data);
+            this.setState({
+                user: this.state.user,
+                text: this.state.text,
+
+            })
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    render() {
+        return (
             <Fragment>
-            <h1>Random text</h1>
+                <h1> hello</h1>
             </Fragment>
+
         )
+
     }
 }
 
